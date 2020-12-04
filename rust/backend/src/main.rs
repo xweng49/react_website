6533b434c203da38ff0d2cf389d5f744718b6b75
ScipioAfricanus1 <bdharris3@uh.edu>
2020-12-04 16:31:53 -0600

    Created call to yahoo_finance_api, set up rest API for calling. AB#8, AB#9

--- rust/backend/src/main.rs
+++ rust/backend/src/main.rs
@@ -1,5 +1,4 @@
 use backend::*;
-use yahoo_finance::history;
 use serde_json::{Value};
 use actix_web::{get, web, App, HttpServer, Responder};
 
@@ -9,6 +8,12 @@ use actix_web::{
 use futures::StreamExt;
 use serde::{Serialize, Deserialize};
 use json::{JsonValue};
+use yahoo_finance_api as yahoo;
+use yahoo::{YahooError, Quote};
+use std::time::{Duration, UNIX_EPOCH};
+use chrono::{Utc,TimeZone};
+use tokio_test;
+
 
 
 // #[get("/{id}/{name}/index.html")]
@@ -55,7 +60,7 @@ async fn index_manual(mut payload: web::Payload) -> Result<HttpResponse, actix_w
     Ok(HttpResponse::Ok().json(obj)) // <- send response
 }
 
-/// This handler manually load request payload and parse json-rust
+/// This handler manually load request payload and parses json-rust
 async fn index_mjsonrust(body: web::Bytes) -> Result<HttpResponse, Error> {
     // body is loaded, now we can deserialize json-rust
     let result = json::parse(std::str::from_utf8(&body).unwrap()); // return Result
@@ -63,18 +68,21 @@ async fn index_mjsonrust(body: web::Bytes) -> Result<HttpResponse, Error> {
         Ok(v) => v,
         Err(e) => json::object! {"err" => e.to_string() },
     };
+    //TODO: replace the unwrap maybe.
+    //TODO: using injson to get quotes:
+    let quotes = get_quotes().await.unwrap();
 
-    let json_response = json::from("{ response: \"Success\" }");
+    // let json_response = json::from(r#"{ response: "Success" }"#);
+    //TODO: process quotes struct so that we can post the response.
+    let processed_quotes = process_quotes(quotes);
     Ok(HttpResponse::Ok()
         .content_type("application/json")
-        .body(json_response.dump()))
+        .body(processed_quotes.dump()))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-   pull_from_postman().unwrap();
-   // grab_apple_info();
-
+//    pull_from_postman().unwrap();
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
@@ -83,15 +91,7 @@ async fn main() -> std::io::Result<()> {
             // enable logger
             .wrap(middleware::Logger::default())
             .data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
-            // .service(web::resource("/extractor").route(web::post().to(index)))
-            // .service(
-            //     web::resource("/extractor2")
-            //         .data(web::JsonConfig::default().limit(1024)) // <- limit size of the payload (resource level)
-            //         .route(web::post().to(extract_item)),
-            // )
-            // .service(web::resource("/manual").route(web::post().to(index_manual)))
             .service(web::resource("/mjsonrust").route(web::post().to(index_mjsonrust)))
-            // .service(web::resource("/").route(web::post().to(index)))
     })
     .bind("127.0.0.1:8080")?
     .run()
@@ -116,38 +116,38 @@ fn pull_from_postman() -> Result<(), Error> {
   Ok(())
 }
 
-fn untyped_example() -> Result<(),Error> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data = r#"
-        {
-            "name": "John Doe",
-            "age": 43,
-            "phones": [
-                "+44 1234567",
-                "+44 2345678"
-            ]
-        }"#;
-
-    // Parse the string of data into serde_json::Value.
-    let v: Value = serde_json::from_str(data)?;
-
-    // Access parts of the data by indexing with square brackets.
-    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
-    Ok(())
+//Todo : user input
+async fn get_quotes() -> Result<Vec<Quote>, YahooError>{
+    let provider = yahoo::YahooConnector::new();
+    //TODO: get the quotes using the json info.
+    let start = Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0);
+    let end = Utc.ymd(2020,1,31).and_hms_milli(23, 59, 59, 999);
+    // returns historic quotes with daily interval
+    let resp = tokio_test::block_on(provider.get_quote_history("AAPL", start, end));
+    let quotes = resp.unwrap().quotes();
+    let quote_vec = match quotes {
+        Ok(string) => string,
+        Err(e) => return Err(e),
+    };
+    println!("Apple's quotes of the last month: {:?}", quote_vec);
+    Ok(quote_vec)
 }
 
-
-//TODO: Either find out how to work around their code or fix it myself or find a new api for finance in rust?
-async fn grab_apple_info() {
-    let number = test_function();
-    println!("Hello world: {}", number);
-
-    // retrieve 6 months worth of data for Apple
-   let result_data = history::retrieve("AAPL").await;
-   let data = result_data.unwrap();
-   // print the date and closing price for each day we have data
-   for bar in &data {
-      // println!("On {} Apple closed at ${:.2}", bar.datetime(), bar.close)
-   }
-
+fn process_quotes(quotes: Vec<Quote>) -> JsonValue {
+    let mut num = 0;
+    let mut jsonQuotes = json::JsonValue::new_array();
+    for  quote in quotes {
+        let mut data = json::JsonValue::new_object();
+        data["timestamp"] = quote.timestamp.into();
+        data["open"] = quote.open.into();
+        data["high"] = quote.high.into();
+        data["low"] = quote.low.into();
+        data["volume"] = quote.volume.into();
+        data["close"] = quote.close.into();
+        data["adjclose"] = quote.adjclose.into();
+        //pushes quote data with quote key into jsonquotes
+        jsonQuotes[format!("Quote {}: ", num)] = data.into();
+        num +=1;
+    }
+    jsonQuotes
 }
