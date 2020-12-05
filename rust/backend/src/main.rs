7f3681eac1f0140f5c821c5c71a6c083ebbbdd6f
ScipioAfricanus1 <bdharris3@uh.edu>
2020-12-05 13:48:59 -0600

    Added input system fixed AB#10

--- rust/backend/src/main.rs
+++ rust/backend/src/main.rs
@@ -12,65 +12,30 @@ use yahoo_finance_api as yahoo;
 use yahoo::{YahooError, Quote};
 use std::time::{Duration, UNIX_EPOCH};
 use chrono::{Utc,TimeZone};
+use log::{info, warn};
 use tokio_test;
 
 
-//Test
-// #[get("/{id}/{name}/index.html")]
-// async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
-//     format!("Hello {}! id:{}", name, id)
-// }
+
 #[derive(Debug, Serialize, Deserialize)]
 struct MyObj {
     name: String,
     number: i32,
 }
 
-/// This handler uses json extractor
-async fn index(item: web::Json<MyObj>) -> HttpResponse {
-    println!("model: {:?}", &item);
-    HttpResponse::Ok().json(item.0) // <- send response
-}
 
-/// This handler uses json extractor with limit
-async fn extract_item(item: web::Json<MyObj>, req: HttpRequest) -> HttpResponse {
-    println!("request: {:?}", req);
-    println!("model: {:?}", item);
-
-    HttpResponse::Ok().json(item.0) // <- send json response
-}
-
-const MAX_SIZE: usize = 262_144; // max payload size is 256k
-
-/// This handler manually load request payload and parse json object
-async fn index_manual(mut payload: web::Payload) -> Result<HttpResponse, actix_web::Error> {
-    // payload is a stream of Bytes objects
-    let mut body = web::BytesMut::new();
-    while let Some(chunk) = payload.next().await {
-        let chunk = chunk?;
-        // limit max size of in-memory payload
-        if (body.len() + chunk.len()) > MAX_SIZE {
-            return Err(actix_web::error::ErrorBadRequest("overflow"));
-        }
-        body.extend_from_slice(&chunk);
-    }
-
-    // body is loaded, now we can deserialize serde-json
-    let obj = serde_json::from_slice::<MyObj>(&body)?;
-    Ok(HttpResponse::Ok().json(obj)) // <- send response
-}
 
 /// This handler manually load request payload and parses json-rust
 async fn index_mjsonrust(body: web::Bytes) -> Result<HttpResponse, Error> {
     // body is loaded, now we can deserialize json-rust
     let result = json::parse(std::str::from_utf8(&body).unwrap()); // return Result
-    let injson: JsonValue = match result {
+    let request: JsonValue = match result {
         Ok(v) => v,
         Err(e) => json::object! {"err" => e.to_string() },
     };
     //TODO: replace the unwrap maybe.
     //TODO: using injson to get quotes:
-    let quotes = get_quotes().await.unwrap();
+    let quotes = get_quotes(request).await.unwrap();
 
     // let json_response = json::from(r#"{ response: "Success" }"#);
     //TODO: process quotes struct so that we can post the response.
@@ -83,9 +48,10 @@ async fn index_mjsonrust(body: web::Bytes) -> Result<HttpResponse, Error> {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 //    pull_from_postman().unwrap();
-    std::env::set_var("RUST_LOG", "actix_web=info");
+    std::env::set_var("RUST_LOG", "info");
     env_logger::init();
-
+    info!("the answer was: {}", "asdofjd");
+    warn!("did this read out");
     HttpServer::new(|| {
         App::new()
             // enable logger
@@ -100,28 +66,22 @@ async fn main() -> std::io::Result<()> {
     
 }
 
-fn pull_from_postman() -> Result<(), Error> {
-   //listens for postman request, reads the response.
-
-   let response = r#"
-   {
-      "name": "company",
-      "date_start": "1-1-2020",
-      "date_end": "12-30-2020"
-  }"#;
-
-  let value: Value = serde_json::from_str(response)?;
-
-  println!("Name: {}, date_start: {}, date_end: {}", value["name"], value["date_start"], value["date_end"]);
-  Ok(())
-}
 
 //Todo : user input
-async fn get_quotes() -> Result<Vec<Quote>, YahooError>{
+///takes the quotes request, calls a method to process the dates into month, day, year in two tuples
+async fn get_quotes(request: JsonValue) -> Result<Vec<Quote>, YahooError>{
     let provider = yahoo::YahooConnector::new();
     //TODO: get the quotes using the json info.
-    let start = Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0);
-    let end = Utc.ymd(2020,1,31).and_hms_milli(23, 59, 59, 999);
+    let name = request["name"].clone().to_string();
+    let date_start = request["date_start"].clone();
+    let date_end = request["date_end"].clone();
+    let (year_start, month_start, day_start) = process_date_ymd(date_start);
+    let (year_end, month_end, day_end) = process_date_ymd(date_end);
+
+    let start = Utc.ymd(year_start, month_start, day_start).and_hms_milli(0, 0, 0, 0);
+    let end = Utc.ymd(year_end, month_end, day_end).and_hms_milli(23, 59, 59, 999);
+    // let start = Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0);
+    // let end = Utc.ymd(2020, 1, 31).and_hms_milli(23, 59, 59, 999);
     // returns historic quotes with daily interval
     let resp = tokio_test::block_on(provider.get_quote_history("AAPL", start, end));
     let quotes = resp.unwrap().quotes();
@@ -150,4 +110,15 @@ fn process_quotes(quotes: Vec<Quote>) -> JsonValue {
         num +=1;
     }
     jsonQuotes
+}
+///processes jsonvalue date.
+fn process_date_ymd(date: JsonValue) -> (i32, u32, u32) {
+    let date_string = date.to_string();
+    let vec_myd: Vec<&str> = date_string.split("-").collect();
+    let month = vec_myd[0].parse::<u32>().unwrap();
+    let year = vec_myd[2].parse::<i32>().unwrap();
+    let day = vec_myd[1].parse::<u32>().unwrap();
+    println!("why doesn't this work?");
+    info!("Month: {:?} Year: {:?} Day: {:?}", month, year, day);
+    (year, month, day)
 }
